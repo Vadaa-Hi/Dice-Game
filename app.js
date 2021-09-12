a4147df44fe3deb5f16b9c06124aa4db8b3ef719
Davaa <m.moo_gii_18@yahoo.com>
2021-09-13 00:55:18 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -9,15 +9,24 @@ var scores = [0,0];
 var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random()*6)+1; 
+
 
 // Програм эхлэхэд бэлтгэе
 // Dom ашиглан ID аар нь хайж оллоо тус бүрийг нь 
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
-window.document.querySelector('#current-0').textContent = 0;
-window.document.querySelector('#current-1').textContent = 0;
-document.querySelector('.dice').style.display ="none";
+document.getElementById("score-0").textContent="0";
+window.document.getElementById('score-1').textContent = "0";
+window.document.getElementById('current-0').textContent = "0";
+window.document.getElementById('current-1').textContent = "0";
 
+var diceDom= document.querySelector('.dice');
 
-console.log('Шооо: '+ dice);
+diceDom.style.display ="none";
+
+document.querySelector('.btn-roll').addEventListener("click", function (){
+    var diceNumber = Math.floor(Math.random()*6)+1; 
+
+    diceDom.style.display ="block";
+    diceDom.src = "dice-"+diceNumber+".png";
+
+    // alert("Шуу буулаа: " +diceNumber)
+});
